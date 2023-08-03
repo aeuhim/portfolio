44cf79e654f914ee10e44cf82788e2f43c721565
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-08-03 16:13:45 +0800

    Add component about section

---
+++ components/AboutSection.tsx
@@ -0,0 +1,76 @@
+export default function AboutSection() {
+  return (
+    <section className="py-5">
+      <div className="container max-w-screen-xl mx-auto px-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-28 md:mb-20">
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../zap.svg" alt="Zap Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Swift Learner
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              Initiated at 12, I embrace new concepts like lightning, adapting swiftly to evolving technologies. My skills stay cutting-edge, an ever-evolving testament to my insatiable thirst for learning.
+            </p>
+          </div>
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../eye.svg" alt="Eye Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Critical Thinker
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              My journey as a developer mirrors visionary thinking. Unveiling innovative solutions like a perceptive eye, I craft digital experiences that captivate users while adhering to a structured process.
+            </p>
+          </div>
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../message-square.svg" alt="Message Square Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Skillful Communicator
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              Beyond code, my clarity bridges technicalities and plain language. Conversing with developers or stakeholders, my seamless communication ensures a flow of ideas and understanding, enriching collaboration.
+            </p>
+          </div>
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../trending-up.svg" alt="Trending Up Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Strong-Willed
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              Anchored by unwavering grit, my journey takes on challenges head-on. Like a mountain against the elements, my strong personality empowers me to create resilient solutions in the dynamic realm of technology.
+            </p>
+          </div>
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../crosshair.svg" alt="Crosshair Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Data-Driven
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              Precision defines me. Like a crosshair, I target every aspect of software design, ensuring flawless alignment. This meticulous approach crafts software of unparalleled quality, paying homage to intricate detail.
+            </p>
+          </div>
+          <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+              <img src="../map.svg" alt="Map Icon" />
+            </div>
+            <h4 className="font-medium text-gray-700 text-lg mb-4">
+              Process-Oriented
+            </h4>
+            <p className="font-normal text-gray-500 text-md">
+              My programming journey maps meticulously. Like a cartographer, I guide projects with a compass. Navigating each phase, projects reach destinations with precision, innovation, and unwavering excellence.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
